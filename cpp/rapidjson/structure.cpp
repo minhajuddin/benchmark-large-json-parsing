d653077732e802b078179d4458ed0b38ef612cd9
OvermindDL1 <overminddl1@gmail.com>
2018-05-24 16:13:07 -0600

    Compile the correct source code...

--- cpp/rapidjson/structure.cpp
+++ cpp/rapidjson/structure.cpp
@@ -8,7 +8,8 @@ using namespace rapidjson;
 int main(int argc, char *argv[]) {
 	char readBuffer[65536];
 	FileReadStream is(stdin, readBuffer, sizeof(readBuffer));
-	if(document.Parse<kParseValidateEncodingFlag>(is).HasParseError()) return 1;
+	Document document;
+	if(!document.ParseStream<kParseValidateEncodingFlag>(is).HasParseError()) return 1;
 	return 0;
 }
 
