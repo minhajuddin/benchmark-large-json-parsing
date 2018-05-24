ba31ff496911167942c5c5859a3ab381be649f92
OvermindDL1 <overminddl1@gmail.com>
2018-05-24 16:21:34 -0600

    Fix boolean test

--- cpp/rapidjson/structure.cpp
+++ cpp/rapidjson/structure.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]) {
 	char readBuffer[65536];
 	FileReadStream is(stdin, readBuffer, sizeof(readBuffer));
 	Document document;
-	if(!document.ParseStream<kParseValidateEncodingFlag>(is).HasParseError()) return 1;
+	if(document.ParseStream<kParseValidateEncodingFlag>(is).HasParseError()) return 1;
 	return 0;
 }
 
